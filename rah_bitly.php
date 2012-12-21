7f5a326b05fbbfd1ce8afd955b3e045c7040097f
Jukka Svahn <web@rahinaa.biz>
2012-12-21 13:55:17 +0200

    Grant access to the 4.6-dev preferences group.

--- rah_bitly.php
+++ rah_bitly.php
@@ -156,6 +156,7 @@ class rah_bitly
 	public function __construct()
 	{
 		add_privs('plugin_prefs.'.__CLASS__, '1,2');
+		add_privs('prefs.'.__CLASS__, '1,2');
 		register_callback(array(__CLASS__, 'prefs'), 'plugin_prefs.'.__CLASS__);
 		register_callback(array(__CLASS__, 'install'), 'plugin_lifecycle.'.__CLASS__);
 		register_callback(array($this, 'initialize'), 'article', '', 1);
